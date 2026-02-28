88b27ef54a72671a9b4226dd3b23fb08bf6e6ef3
enggehad424 <enggehad424@gmail.com>
2026-02-28 03:19:00 +0200

    finish onboarding screens

---
+++ lib/features/splash/view/splash_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+///class SplashScreen extends StatelessWidget{
+///const SplashScreen({super.key});
+import 'dart:async';
+
+import '../../../core/utilit/colors_app.dart'; // مهم للـ Timer
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // هنا بنعمل Timer
+    Timer(const Duration(seconds: 3), () {
+      // بعد 3 ثواني نعمل navigate للشاشة التالية
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.lightBlackColor,
+      body:
+      SafeArea(
+        child: Column(
+          children: [
+            const Spacer(),  // يأخذ مساحة فارغة من الأعلي
+            Center(
+              // يضمن ان الشعار في منتصف العرض تماما
+              child: Image.asset('assets/images/Group 44.png',
+                width: MediaQuery.of(context).size.width*0.6,
+              ),),
+            Spacer(),
+            Image.asset('assets/images/Mask group.png',
+              width: 120,),
+            const SizedBox(height: 16),
+            const Text(
+              'Supervised by Mohamed Nabil',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight:FontWeight.w400,
+              ),
+
+            ),
+
+
+            const SizedBox(height: 24,),
+          ],
+        ),
+      ),
+    );
+
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'core/utilit/app_theme.dart';
+import 'features/splash/view/onboarding_page.dart';
+import 'features/splash/view/splash_screen.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(const MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'New GP App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Movies App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        OnboardingPage.routeName: (context) => const OnboardingPage(),
+        //Onboarding4.routeName: (context) => const Onboarding4(),
+      },
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _increment() => setState(() => _counter++);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('New GP App')),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('Welcome to the team starter app!'),
-            const SizedBox(height: 8),
-            Text('Button pressed: \\$_counter times', style: const TextStyle(fontSize: 16)),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _increment,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
