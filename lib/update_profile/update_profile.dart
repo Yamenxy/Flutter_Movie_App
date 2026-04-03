75cd28c0e26c843b0b2f71a6736a2afbef3b38e6
Salma <salmaalesawy433@gmail.com>
2026-04-03 18:46:57 +0200

    update profile logic add movies list to home screen

--- lib/update_profile/update_profile.dart
+++ lib/update_profile/update_profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/utils/CloudFireStoreUtils.dart';
+import 'package:movies_app/models/profileModel.dart';
 
 import '../core/gen/assets.gen.dart';
 import '../core/theme/ColorPalette.dart';
@@ -12,86 +14,172 @@ class UpdateProfile extends StatefulWidget {
   @override
   State<UpdateProfile> createState() => _UpdateProfileState();
 }
+
 ///
 
 class _UpdateProfileState extends State<UpdateProfile> {
-  Widget? selectedImage;
+  bool isInitialized = false;
+  Profilemodel? profilemodel;
+  late TextEditingController nameController;
+  late TextEditingController phoneController;
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController();
+    phoneController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    phoneController.dispose();
+    super.dispose();
+  }
+
+  bool isEnabled = false;
+  String? selectedImage;
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
         title: Text("Pick Avatar"),
-        leading: Icon(Icons.arrow_back_sharp),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_sharp),
+        ),
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 40),
-            CircleAvatar(
-              radius: 100,
-              child: InkWell(
-                overlayColor: WidgetStatePropertyAll(Colors.transparent),
-                onTap: () {
-                  setState(() {
-                    _showGridBottomSheet(context);
-                  });
-                },
-                child: selectedImage ?? Assets.images.profile1.image(),
-              ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
+      body: StreamBuilder<Profilemodel?>(
+        stream: Cloudfirestoreutils.getProfileStream("user1"),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data == null) {
+            return Center(child: Text("No Data Exists"));
+          }
+          final profile = snapshot.data;
+          if (!isInitialized) {
+            nameController.text = profile?.name ?? "";
+            phoneController.text = profile?.phone ?? "";
+            isInitialized = true;
+          }
+          return Center(
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Customtextformfield(
-                      text: "John Safwat",
-                      icon: Assets.icons.userIcn.svg(),
-                    ),
-                    SizedBox(height: 20),
-                    Customtextformfield(
-                      text: "01200000000",
-                      icon: Assets.icons.phoneIcn.svg(),
-                    ),
-                    SizedBox(height: 20),
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
+                    SizedBox(height: 40),
+                    CircleAvatar(
+                      radius: 100,
+                      child: InkWell(
                         overlayColor: WidgetStatePropertyAll(
                           Colors.transparent,
                         ),
+                        onTap: () {
+                          setState(() {
+                            _showGridBottomSheet(context);
+                          });
+                        },
+                        child: selectedImage == null
+                            ? (profile?.image!=null? Image.asset(profile!.image!):Assets.images.profile1.image()):Image.asset(selectedImage!),
                       ),
-                      child: Text(
-                        "Reset Password",
-                        style: textTheme.titleLarge,
+                    ),
+                    SizedBox(height: 20),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Customtextformfield(
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return "Enter name";
+                                }
+                                return null;
+                              },
+                              controller: nameController,
+                              icon: Assets.icons.userIcn.svg(),
+                            ),
+                            SizedBox(height: 20),
+                            Customtextformfield(
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return "Enter your phone";
+                                }
+                                return null;
+                              },
+                              controller: phoneController,
+                              icon: Assets.icons.phoneIcn.svg(),
+                            ),
+                            SizedBox(height: 20),
+                            TextButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                overlayColor: WidgetStatePropertyAll(
+                                  Colors.transparent,
+                                ),
+                              ),
+                              child: Text(
+                                "Reset Password",
+                                style: textTheme.titleLarge,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Customelevatedbutton(
+                            backGroundColor: Colorpalette.red,
+                            buttonText: "Delete Account",
+                            onPressed: () {},
+                            forGroundColor: Colorpalette.mainTextColor,
+                          ),
+                          SizedBox(height: 16),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0),
+                            child: Customelevatedbutton(
+                              backGroundColor: Colorpalette.yellow,
+                              buttonText: "Update Data",
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  Profilemodel data = Profilemodel(
+                                    name: nameController.text,
+                                    phone: phoneController.text,
+                                    image: selectedImage,
+                                    id: "user1",
+                                  );
+                                      await Cloudfirestoreutils.updateProfile(
+                                        data,
+                                      );
+                                  Navigator.pop(context, selectedImage);
+                                }
+                              },
+                              forGroundColor: Colorpalette.primaryColor,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-            Customelevatedbutton(
-              backGroundColor: Colorpalette.red,
-              buttonText: "Delete Account",
-              onPressed: () {},
-              forGroundColor: Colorpalette.mainTextColor,
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Customelevatedbutton(
-                backGroundColor: Colorpalette.yellow,
-                buttonText: "Update Data",
-                onPressed: () {},
-                forGroundColor: Colorpalette.primaryColor,
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
